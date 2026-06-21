99ac5db61330af757921fbdf4b037e1be8577848
mazen-eltiyab <mazinn2006@gmail.com>
2026-06-21 16:59:11 +0300

    Force exception rendering

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,5 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->render(function (\Throwable $e) {
+            return response("<pre>" . (string) $e . "</pre>", 500);
+        });
     })->create();
