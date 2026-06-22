2433b1bb3c165ed4437b809cf4df097d5a2905aa
mazen-eltiyab <mazinn2006@gmail.com>
2026-06-22 16:13:26 +0300

    Auto clear config cache on boot for Railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (file_exists(app()->getCachedConfigPath())) {
+            @unlink(app()->getCachedConfigPath());
+        }
+
         if (env('APP_ENV') !== 'local') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
         }
